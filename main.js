f376d1a0cb50eec007312bfdea97a0cdadd04eed
BigMoneyBones <87784559+BigMoneyBones@users.noreply.github.com>
2022-02-15 20:27:51 -0600

    update

---
+++ main.js
@@ -0,0 +1,72 @@
+// console.log(xify("hello"));
+
+// function xify(str) {
+//     let strX = "";
+//     str.length
+//     for (let count = 0; count < str.length; count++){
+//         strX += 'x';
+//     }
+//     return strX;
+// }
+
+
+        // function yellingChars(str) {
+        //     let strY = "";
+        //     for (let count = 0; count < str.length; count++) {
+        //         strY = strY + str[count] + "!";
+
+        //     }
+        //     console.log(strY);
+        // }
+
+        // let str1 = 'goodness';
+        // yellingChars(str1);
+
+
+// function indexedChars(str) {
+//     let strA = "";
+//     for (count = 0; count < str.length; count++) {
+//         strA = strA + count + str[count];
+//     }
+//     console.log(strA);
+// }    
+// let str1 = 'hello';
+// indexedChars(str1);
+
+
+//***OR*** 
+
+// function indexedChars(str) {
+//     let strA = "";
+//     let index = 0;
+//     for (let i of str) {
+//         strA += index;
+//         strA += i;
+//         index = index +1;
+//     }
+//     console.log(strA);
+// }
+
+// let strA = 'hello';
+// indexedChars(strA)
+
+
+        // function numberedChars(str) {
+        //     let strA = '';
+        //     let index = 1;
+        //     for (let i of str) {
+        //         strA += "(" + index + ")";
+        //         strA += (i);
+        //         index = index + 1
+        //     }
+        //     console.log(strA);
+        // }
+
+        // let str1 = 'hello'
+        // numberedChars(str1);
+
+
+function exclaim(str) {
+    let strA = '';
+
+}
